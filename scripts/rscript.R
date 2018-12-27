97f0fee0788e7ad1383f0f87bd228537e29d1adb
nilforooshan <m.a.nilforooshan@gmail.com>
2018-12-27 14:05:55 +1300

    Different output directory

--- scripts/rscript.R
+++ scripts/rscript.R
@@ -1,6 +1,6 @@
 rline = readLines("/mnt/data/datfile")
-dir.create("/mnt/data/results/")
-utfile = "/mnt/data/results/result"
+dir.create("/mnt/data/results_R/")
+utfile = "/mnt/data/results_R/result"
 system(paste("echo VERSION: $VERSION >", utfile))
 write(paste("The data has", nchar(rline), "Characters."), file=utfile, append=TRUE)
 write(date(), file=utfile, append=TRUE)
